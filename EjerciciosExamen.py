cb3bb7804926c0c63c242ae12899016b427dfca6
20sync <sebastianalban20@gmail.com>
2026-07-02 13:24:02 -0400

    Clase 02-07-26

---
+++ EjerciciosExamen.py
@@ -0,0 +1,19 @@
+vehiculos={
+
+    'A001' : {'Corsa','Chevrolet','2009','4'}
+
+}
+
+op={
+
+    'A001' : {'02-07-26', 'Pendiente'}
+
+}
+
+def buscarAutos():
+    print()
+
+def mostrarAutos(dic):
+    for codigo, values in dic.items():
+        print(f"{codigo}.-  {values}")
+
